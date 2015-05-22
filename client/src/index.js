3f5fc2895b767e880c5c104d41d4bf5388b71077
Shimon Mochizuki <shimon.no.mail@gmail.com>
2015-05-22 12:52:40 +0900

    Completed to channels guide

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,25 @@
 import {Socket} from './vendor/phoenix'
 
 window.addEventListener('load', () => {
+  let chatInput         = $("#chat-input")
+  let messagesContainer = $("#messages")
+
   let socket = new Socket("/ws")
   socket.connect()
   let chan = socket.chan("rooms:lobby", {})
+
+  chatInput.on("keypress", event => {
+    if(event.keyCode === 13){
+      chan.push("new_msg", {body: chatInput.val()})
+      chatInput.val("")
+    }
+  })
+
+  chan.on("new_msg", payload => {
+    messagesContainer.append(`<br/>[${Date()}] ${payload.body}`)
+  })
+
   chan.join().receive("ok", chan => {
     console.log("Welcome to Phoenix Chat!")
-  });
+  })
 });
